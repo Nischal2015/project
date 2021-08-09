5876ffba3a0a9de927644510700b62e628a6ddc5
Nischal2015 <nischalshakya2015@gmail.com>
2021-08-09 07:52:02 +0545

    fixed all links

--- allstudents.php
+++ allstudents.php
@@ -92,7 +92,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true) {
                 <div class='row'>
                     <div class='col-md-12'>
                         <div class='alert alert-success alert-dismissible fade show' role='alert'>
-                            <strong>Success!</strong> Your student has been udpated succesfully.
+                            <strong>Success!</strong> Your student has been updated succesfully.
                         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                       </div>
                     </div>
