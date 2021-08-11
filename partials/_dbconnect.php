c3290840183caf12962012d0381e9a34d7ba019d
Nischal2015 <nischalshakya2015@gmail.com>
2021-08-11 14:27:45 +0545

    deployment code updated

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,14 +1,14 @@
 <!-- Establishing connection with the database -->
 <?php
-// $server="remotemysql.com";
-// $username="GfYJT7egTF";
-// $password="lW1UnyRWzL";
-// $database="GfYJT7egTF";
+$server="remotemysql.com";
+$username="GfYJT7egTF";
+$password="lW1UnyRWzL";
+$database="GfYJT7egTF";
 
-$server="localhost";
-$username="root";
-$password="";
-$database="thes_db";
+// $server="localhost";
+// $username="root";
+// $password="";
+// $database="thes_db";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 
