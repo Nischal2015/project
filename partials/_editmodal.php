858f9aae7aabbc001ec63b0ec82db22693ef41b6
Nischal2015 <nischalshakya2015@gmail.com>
2021-08-11 12:55:53 +0545

    fixed styling

--- partials/_editmodal.php
+++ partials/_editmodal.php
@@ -4,15 +4,11 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="studentEditModalLabel">Edit Information</h5>
-                <span>
-                    <i class="material-icons close-btn" data-bs-dismiss="modal" aria-label="Close">
-                        close
-                    </i>
-                </span>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="<?php $_SERVER['REQUEST_URI']; ?>" method="post">
-                    <input class="hidden" name="snoEdit" id="snoEdit" style="display: none;">
+                <input class="hidden" name="snoEdit" id="snoEdit" style="display: none;">
                     <div class="row">
                         <div class="col-md-6 pt-4">
                             <div class="form-floating mb-2">
