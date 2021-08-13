1a11a6368943def0df772a342181c9efdcdb7a93
Nischal2015 <nischalshakya2015@gmail.com>
2021-08-13 14:57:43 +0545

    fixed button

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,13 +1,13 @@
 <!-- Establishing connection with the database -->
 <?php
-// $server="remotemysql.com";
-// $username="GfYJT7egTF";
-// $password="lW1UnyRWzL";
-// $database="GfYJT7egTF";
-$server="localhost";
-$username="root";
-$password="";
-$database="thes_db";
+$server="remotemysql.com";
+$username="GfYJT7egTF";
+$password="lW1UnyRWzL";
+$database="GfYJT7egTF";
+// $server="localhost";
+// $username="root";
+// $password="";
+// $database="thes_db";
 $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn) {
 	die("Error " . mysqli_connect_error());
